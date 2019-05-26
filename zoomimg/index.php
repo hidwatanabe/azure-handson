bd8b007411ba0681536172a737a6ac7539373237
hacker3 <hacker3@OTAPRD477ops.onmicrosoft.com>
2019-05-26 14:51:13 +0900

    initial app

---
+++ zoomimg/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Picture Demo</title>
+        <link href="site.css" rel="stylesheet" />
+    </head>
+    <body>
+        <h1>Hello Azure Web Sites</h1>
+        <img id="bouncingImage" src="bird.jpg" alt="Picture" >
+    </body>
+</html>
