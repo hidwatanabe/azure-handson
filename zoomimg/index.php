0759401d6da5daf9eab99b7d33dbb1618cae1486
hacker3 <hacker3@OTAPRD477ops.onmicrosoft.com>
2019-05-27 08:31:16 +0900

    modified

--- zoomimg/index.php
+++ zoomimg/index.php
@@ -9,7 +9,6 @@
     <body style="background-color:#d3d3d3;">
         <div style="text-align:center;">
             <h1>Hello Azure Web Sites</h1>
-            <br>
             <img id="bouncingImage" src="./1.jpg" alt="Picture" style="margin-top:2em;">
         </div>
     </body>
